a1c257662dbdf9d8adf25d2ee0fa53df0c5de745
Adam <adamkerbel6@gmail.com>
2025-03-09 15:54:10 +0000

    Edited Server.py

--- Back end/Server.py
+++ Back end/Server.py
@@ -6,6 +6,7 @@ import atexit
 
 app = Flask(__name__)
 CORS(app) #nb if we get cors-related errors after this disable this
+app.config['UPLOAD FOLDER']
 
 @app.route('/process', methods=['POST'])
 def process_data():
